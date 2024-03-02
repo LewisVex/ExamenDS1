acf9b58b2d25eac4afca3354830a1a811ab228cd
MrCeoj <ortizludwin68@outlook.com>
2024-03-01 20:38:55 -0800

    Marcelo - Alta profesores + Usuario
    Se terminó el formulario de altas para estas entidades

--- negocio/src/main/java/mx/monkeysoft/facade/FacadeProfesor.java
+++ negocio/src/main/java/mx/monkeysoft/facade/FacadeProfesor.java
@@ -5,6 +5,9 @@
  */
 package mx.monkeysoft.facade;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import mx.monkeysoft.delegate.DelegateProfesor;
 import mx.monkeysoft.entidad.Profesor;
 
@@ -12,16 +15,18 @@ import mx.monkeysoft.entidad.Profesor;
  *
  * @author monkeysoft
  */
+@Stateless
 public class FacadeProfesor {
-    
+
+    @PersistenceContext
+    private EntityManager em;
     private final DelegateProfesor delegateProfesor;
 
     public FacadeProfesor() {
         this.delegateProfesor = new DelegateProfesor();
     }
-    
-    public void guardarProfesor(Profesor profesor){
+
+    public void guardarProfesor(Profesor profesor) {
         delegateProfesor.saveProfesor(profesor);
     }
-    
 }
