8173a57b9d67859c13b85b01ccfdfb0751e58f5c
Luis Vega <HonorVex03@gmail.com>
2024-03-04 03:08:03 -0800

    Apartado visual Alta UA
    Adaptación de los nuevos estilos a la vista de la Alta UA.
    Se ha restablecido y actualizado el archivo Profesores.xhtml (Vista de Profesores en Unidad de Aprendizaje)

--- entidad/target/generated-sources/annotations/mx/monkeysoft/entidad/Users_.java
+++ entidad/target/generated-sources/annotations/mx/monkeysoft/entidad/Users_.java
@@ -4,7 +4,6 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-03-03T20:42:12")
 @Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-03-04T02:00:48")
 @StaticMetamodel(Users.class)
 public class Users_ { 
